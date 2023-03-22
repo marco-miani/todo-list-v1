9bdd47748e50e0763671153b609aa85e6b94c36d
Marco Miani <mmiani21@gmail.com>
2023-03-22 05:55:21 -0300

    first commit

---
+++ date.js
@@ -0,0 +1,30 @@
+//jshint esversion:6
+
+exports.getDate = function () {
+
+    let today = new Date();
+
+    let options = {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric'
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    return day;
+};
+
+
+exports.getDay = function () {
+
+    let today = new Date();
+
+    let options = {
+        day: 'numeric'
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    return day;
+};
